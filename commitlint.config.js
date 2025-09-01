800ca9228907b71cf45fba69e154c8dde1a9a9b5
Pooja Shivach <pshivach@costco.com>
2025-09-01 14:13:53 +0530

    feat: add navbar component

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-pattern': [2, 'always', /^(\w+): (\w+): (.+)$/],
-    'header-max-length': [2, 'always', 100],
+    // 'header-pattern': [2, 'always', /^(\w+): (\w+): (.+)$/],
+    // 'header-max-length': [2, 'always', 100],
     'type-enum': [
       2,
       'always',
